6e7e32e23c4a1ee94a3df830bdc91f95d250e492
Jeff <Jeff>
2022-03-15 22:42:00 -0400

    correct wrong image

--- app/imgs_dic.js
+++ app/imgs_dic.js
@@ -1,5 +1,5 @@
 img_sources = {
-    "cheese_tea": "https://www.debutea.com/uploads/1/3/1/2/131261819/s264673473781446249_c4_i2_w5760.jpeg?width=640?width=800",
+    cheese_tea: "https://www.debutea.com/uploads/1/3/1/2/131261819/s264673473781446249_c3_i7_w2220.jpeg?width=640?width=800",
     fresh_fruit_tea: "https://www.debutea.com/uploads/1/3/1/2/131261819/s264673473781446249_c4_i2_w5760.jpeg?width=640?width=800",
     fresh_milk_tea_latte: "https://www.debutea.com/uploads/1/3/1/2/131261819/s264673473781446249_c10_i2_w5273.jpeg?width=640?width=800"
 }
